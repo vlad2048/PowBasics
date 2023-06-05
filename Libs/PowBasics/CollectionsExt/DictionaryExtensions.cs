8751cde0611be09fcd69f2373945c90d730a3107
Vlad Niculescu <vlad.nic@gmail.com>
2023-06-05 17:54:22 +0100

    Removed Pow.WinForms. Added MapKeys and MapValues Dictionary extension functions

--- Libs/PowBasics/CollectionsExt/DictionaryExtensions.cs
+++ Libs/PowBasics/CollectionsExt/DictionaryExtensions.cs
@@ -2,6 +2,35 @@
 
 public static class DictionaryExtensions
 {
+	/// <summary>
+	/// Map the keys in a Dictionary
+	/// </summary>
+	public static IReadOnlyDictionary<K2, V> MapKeys<K1, K2, V>(this IReadOnlyDictionary<K1, V> dict, Func<K1, K2> fun)
+		where K1 : notnull
+		where K2 : notnull
+	{
+		var res = new Dictionary<K2, V>();
+		foreach (var (key, val) in dict)
+			res[fun(key)] = val;
+		return res;
+	}
+
+	/// <summary>
+	/// Map the values in a Dictionary
+	/// </summary>
+	public static IReadOnlyDictionary<K, V2> MapValues<K, V1, V2>(this IReadOnlyDictionary<K, V1> dict, Func<V1, V2> fun)
+		where K : notnull
+	{
+		var res = new Dictionary<K, V2>();
+		foreach (var (key, val) in dict)
+			res[key] = fun(val);
+		return res;
+	}
+
+
+	/// <summary>
+	/// Get a value from a Dictionary based on a key, if it doesn't exist, create and add it using the function provided
+	/// </summary>
 	public static TValue GetOrCreate<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, Func<TValue> createFun)
 		where TKey : notnull
 	{
