8719967dc216499ca4b3cb3ef7061c946fb409fe
Vlad Niculescu <vlad.nic@gmail.com>
2023-07-05 22:35:58 +0100

    internal -> public

--- Libs/PowBasics/ColorCode/Structs/Txt.cs
+++ Libs/PowBasics/ColorCode/Structs/Txt.cs
@@ -6,7 +6,7 @@ public sealed record Txt(
 	TxtChunk[][] Lines
 )
 {
-	internal static Txt FromChunk(string text, Color color) => new(new[]
+	public static Txt FromChunk(string text, Color color) => new(new[]
 	{
 		new[] { new TxtChunk(text, color) }
 	});
