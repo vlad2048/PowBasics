f2b53335be8a7d1d183092bc8ed23d7dfa640119
Vlad Niculescu <vlad.nic@gmail.com>
2023-07-05 22:33:50 +0100

    ext

--- Libs/PowBasics/ColorCode/Structs/Txt.cs
+++ Libs/PowBasics/ColorCode/Structs/Txt.cs
@@ -11,3 +11,18 @@ public sealed record Txt(
 		new[] { new TxtChunk(text, color) }
 	});
 }
+
+
+public static class TxtExt
+{
+	public static (int, int) GetSize(this Txt txt) => txt.Lines.All(e => e.GetLng() == 0) switch
+	{
+		true => (0, 0),
+		false => (
+			txt.Lines.Max(e => e.GetLng()),
+			txt.Lines.Length
+		)
+	};
+
+	private static int GetLng(this IEnumerable<TxtChunk> line) => line.Sum(e => e.Text.Length);
+}
