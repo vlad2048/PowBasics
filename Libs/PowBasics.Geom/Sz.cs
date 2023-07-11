00f79f3b6946be0c034a67cbe9142128e88b0d01
Vlad Niculescu <vlad.nic@gmail.com>
2023-07-11 21:48:49 +0100

    Added Sz operator +

--- Libs/PowBasics.Geom/Sz.cs
+++ Libs/PowBasics.Geom/Sz.cs
@@ -17,6 +17,7 @@ public readonly record struct Sz
 
 	public override string ToString() => $"{Width}x{Height}";
 
+	public static Sz operator +(Sz a, Sz b) => new(a.Width + b.Width, a.Height + b.Height);
 	public static Sz operator -(Sz a, Sz b) => new(Math.Max(0, a.Width - b.Width), Math.Max(0, a.Height - b.Height));
 	public static Sz operator *(Sz a, int z) => new(a.Width * z, a.Height * z);
 	public static Sz operator *(Sz a, double z) => new((int)(a.Width * z), (int)(a.Height * z));
