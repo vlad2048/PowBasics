a797a6b0dd5a6753a7cafb3e5cd93470c79353d2
Vlad Niculescu <vlad.nic@gmail.com>
2023-08-22 22:04:34 +0100

    added Shuffle

---
+++ Libs/PowBasics/CollectionsExt/IEnumerableShuffleExt.cs
@@ -0,0 +1,21 @@
+﻿namespace PowBasics.CollectionsExt;
+
+public static class IEnumerableShuffleExt
+{
+	public static T[] Shuffle<T>(this IEnumerable<T> source, int? seed)
+	{
+		var rnd = seed switch
+		{
+			not null => new Random(seed.Value),
+			null => new Random((int)DateTime.Now.Ticks)
+		};
+		var array = source.ToArray();
+		var n = array.Length;
+		for (var i = 0; i < (n - 1); i++)
+		{
+			var r = i + rnd.Next(n - i);
+			(array[r], array[i]) = (array[i], array[r]);
+		}
+		return array;
+	}
+}
