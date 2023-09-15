d9f171d816567c7bc904fc224fc05b73d9fe4ce5
Vlad Niculescu <vlad.nic@gmail.com>
2023-09-15 22:44:56 +0100

    added Parallelize

---
+++ Libs/PowBasics/Concurrency/Parallelize.cs
@@ -0,0 +1,24 @@
+﻿using PowBasics.CollectionsExt;
+using System.Collections.Concurrent;
+
+namespace PowBasics.Concurrency;
+
+public static class Parallelize
+{
+	public static U[] Run<T, U>(T[] arr, int maxParallelism, Func<T, Task<U>> fun)
+	{
+		var queue = MakeQueue(arr.Length);
+		var res = new U[arr.Length];
+		var tasks = Enumerable
+			.Range(0, maxParallelism)
+			.SelectToArray(_ => Task.Run(async () =>
+			{
+				while (queue.TryDequeue(out var idx))
+					res[idx] = await fun(arr[idx]);
+			}));
+		Task.WaitAll(tasks);
+		return res;
+	}
+	
+	private static ConcurrentQueue<int> MakeQueue(int count) => new(Enumerable.Range(0, count));
+}
