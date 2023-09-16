f8afea46227f4ea92668a151092d78fc5ab7bd0f
Vlad Niculescu <vlad.nic@gmail.com>
2023-09-16 06:05:47 +0100

    added MakeTempFolder

--- Libs/PowBasics/Files/FileUtils.cs
+++ Libs/PowBasics/Files/FileUtils.cs
@@ -8,4 +8,10 @@ public static class FileUtils
 	/// <param name="ext">Desired extension or null for no extension</param>
 	/// <returns>Temporary filename</returns>
 	public static string MakeTempFile(string? ext = null) => Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}{ext}");
+
+	/// <summary>
+	/// Creates an empty temporary folder
+	/// </summary>
+	/// <returns>Temporary folder</returns>
+	public static string MakeTempFolder() => Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}").MakeFolderIFN();
 }
