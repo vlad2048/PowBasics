30f514e6844575a5e88c8662e255220141f02307
Vlad Niculescu <vlad.nic@gmail.com>
2023-09-16 01:23:37 +0100

    added FileExt

---
+++ Libs/PowBasics/Files/FileExt.cs
@@ -0,0 +1,19 @@
+﻿using Directory = System.IO.Directory;
+
+namespace PowBasics.Files;
+
+public static class FileExt
+{
+	public static string MakeFolderIFN(this string folder)
+	{
+		if (!Directory.Exists(folder))
+			Directory.CreateDirectory(folder);
+		return folder;
+	}
+
+	public static string MakeFolderForFileIFN(this string file)
+	{
+		(Path.GetDirectoryName(file) ?? throw new IOException($"Filename '{file}' doesn't have a directory component")).MakeFolderIFN();
+		return file;
+	}
+}
