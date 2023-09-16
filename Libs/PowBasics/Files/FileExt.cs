8c7d9fa181fbfd537864e9badff86bad393977d7
Vlad Niculescu <vlad.nic@gmail.com>
2023-09-16 03:27:43 +0100

    added xml doc

--- Libs/PowBasics/Files/FileExt.cs
+++ Libs/PowBasics/Files/FileExt.cs
@@ -4,6 +4,11 @@ namespace PowBasics.Files;
 
 public static class FileExt
 {
+	/// <summary>
+	/// Creates a folder if it doesn't exist
+	/// </summary>
+	/// <param name="folder">Folder to create</param>
+	/// <returns>Folder given</returns>
 	public static string MakeFolderIFN(this string folder)
 	{
 		if (!Directory.Exists(folder))
@@ -11,6 +16,12 @@ public static class FileExt
 		return folder;
 	}
 
+	/// <summary>
+	/// Creates the folder corresponding to the folder component of the file given
+	/// </summary>
+	/// <param name="file">File</param>
+	/// <returns>File given</returns>
+	/// <exception cref="IOException">If the file doesn't have a folder component</exception>
 	public static string MakeFolderForFileIFN(this string file)
 	{
 		(Path.GetDirectoryName(file) ?? throw new IOException($"Filename '{file}' doesn't have a directory component")).MakeFolderIFN();
--- Libs/PowBasics/Files/FileUtils.cs
+++ Libs/PowBasics/Files/FileUtils.cs
@@ -2,5 +2,10 @@
 
 public static class FileUtils
 {
+	/// <summary>
+	/// Creates a temporary filename that is guaranteed not to exist
+	/// </summary>
+	/// <param name="ext">Desired extension or null for no extension</param>
+	/// <returns>Temporary filename</returns>
 	public static string MakeTempFile(string? ext = null) => Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}{ext}");
 }
