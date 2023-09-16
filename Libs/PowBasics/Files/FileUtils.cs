114c5f366859468aee120b12bc254c42333adeb4
Vlad Niculescu <vlad.nic@gmail.com>
2023-09-16 03:23:07 +0100

    added FileUtils.MakeTempFile

---
+++ Libs/PowBasics/Files/FileUtils.cs
@@ -0,0 +1,6 @@
+﻿namespace PowBasics.Files;
+
+public static class FileUtils
+{
+	public static string MakeTempFile(string? ext = null) => Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}{ext}");
+}
