67bd6388a90ba9315f6f38a32e5f6f34af024597
Vlad Niculescu-Bolintin <vlad.nic@gmail.com>
2024-06-30 02:01:51 +0100

    Multi targetting .net6 /.net8 Cleanup

--- Libs/PowBasics.Geom/README.md
+++
@@ -1,1 +0,0 @@
-﻿# PowBasics.Geom
--- package.bat
+++ package.bat
@@ -1,6 +1,7 @@
-set ver=0.1.6
-set slnFolder=C:\dev\nuget\libs\PowBasics
-set nugetFolder=C:\Users\vlad.niculescu\.nuget\packages
+rem nuget setapikey xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -Source https://api.nuget.org/v3/index.json
+set ver=0.2.0
+set slnFolder=D:\dev\nuget\PowBasics
+set nugetFolder=C:\Users\admin\.nuget\packages
 set nugetUrl=https://api.nuget.org/v3/index.json
 
 
